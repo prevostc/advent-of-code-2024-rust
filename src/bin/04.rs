6f0c55c46eb259374caf3248e8c7c6cd04743fed
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-12-04 10:07:42 +0100

    Cleanup day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -14,15 +14,10 @@ pub fn part_one(input: &str) -> Option<u32> {
         .filter(|&(_, c, _)| c == 'X')
         .filter(|&(point, _, d)| grid.is_in_bounds(point + (d * 3)))
         .filter(|&(point, _, d)| {
-            let x_pos = point;
-            let m_pos = x_pos + d;
-            let a_pos = m_pos + d;
-            let s_pos = a_pos + d;
-            let spells_xmas = grid[x_pos] == 'X'
-                && grid[m_pos] == 'M'
-                && grid[a_pos] == 'A'
-                && grid[s_pos] == 'S';
-
+            let spells_xmas = grid[point] == 'X'
+                && grid[point + d] == 'M'
+                && grid[point + d * 2] == 'A'
+                && grid[point + d * 3] == 'S';
             spells_xmas
         })
         .count();
@@ -37,7 +32,7 @@ pub fn part_two(input: &str) -> Option<u32> {
         .iter_item_and_position()
         .filter(|&(_, &c)| c == 'A')
         .filter(|&(point, _)| {
-            let (Some(&top_left), Some(&top_right), Some(&bot_left), Some(&bot_right)) = (
+            let (Some(&tl), Some(&tr), Some(&bl), Some(&br)) = (
                 grid.get_item(point + UP + LEFT),
                 grid.get_item(point + UP + RIGHT),
                 grid.get_item(point + DOWN + LEFT),
@@ -46,11 +41,9 @@ pub fn part_two(input: &str) -> Option<u32> {
                 return false;
             };
 
-            let spells_xmas_diag1 =
-                (top_left == 'M' && bot_right == 'S') || (top_left == 'S' && bot_right == 'M');
-            let spells_xmas_diag2 =
-                (top_right == 'M' && bot_left == 'S') || (top_right == 'S' && bot_left == 'M');
-            spells_xmas_diag1 && spells_xmas_diag2
+            let diag1 = (tl == 'M' && br == 'S') || (tl == 'S' && br == 'M');
+            let diag2 = (tr == 'M' && bl == 'S') || (tr == 'S' && bl == 'M');
+            diag1 && diag2
         })
         .count();
 
